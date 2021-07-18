8935974a24e67a080b8b652222516a6b2f5f7ef4
Johannes Jakob <hi@johjakob.com>
2021-07-18 15:40:41 +0200

    p5.js: Add parallax effect to `Star`
    - Adds a parallax effect to `Star`
    - Adds improvements for higher movement speed

--- js/star.js
+++ js/star.js
@@ -4,12 +4,15 @@ class Star {
     this.y = random(-height, height);
     this.z = random(width);
     this.pz = this.z;
+
     this.hue = random(220, 370);
     this.saturation = random(20, 50);
     this.lightness = random(50, 80);
 
+    this.parallaxFactor = .0005;
+
     this.update = function () {
-      this.z = this.z - speed;
+      this.z = this.z - starSpeed;
       
       if (this.z < 1) {
         this.z = width;
@@ -17,6 +20,9 @@ class Star {
         this.y = random(-height, height);
         this.pz = this.z;
       }
+
+      this.x += map(mouseX, 0, width, -this.parallaxFactor * this.z, this.parallaxFactor * this.z);
+      this.y += map(mouseY, 0, height, -this.parallaxFactor * this.z, this.parallaxFactor * this.z);
     };
 
     this.show = function () {
@@ -32,7 +38,9 @@ class Star {
       var sy = map(this.y / this.z, 0, 1, 0, height);
       var r = map(this.z, 0, width, radius, 0);
 
-      ellipse(sx, sy, r, r);
+      if (starSpeed < 50) {
+        ellipse(sx, sy, r, r);
+      }
 
       var px = map(this.x / this.pz, 0, 1, 0, width);
       var py = map(this.y / this.pz, 0, 1, 0, height);
@@ -40,7 +48,8 @@ class Star {
       this.pz = this.z;
 
       stroke(this.hue, this.saturation, this.lightness, opacity);
-      strokeWeight(radius / 2);
+      
+      strokeWeight(r / 2);
       line(px, py, sx, sy);
     };
   }
