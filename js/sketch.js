643f21a4cb875a6e31be35c08a58ba417f677150
Johannes Jakob <hi@johjakob.com>
2021-07-19 21:15:30 +0200

    Pages: Add content
    - Adds content to index page
    - Updates styles
    - Updates p5.js sketch

--- js/sketch.js
+++ js/sketch.js
@@ -1,9 +1,15 @@
 var stars = [];
-var starCount = 2000;
-var speed = 20;
+var starCount;
+var starSpeed = 1;
+
+var headingHue = 0;
+
+let starEasing = 0.02;
 
 function setup() {
-  createCanvas(screen.width, screen.height);
+  createCanvas(windowWidth, windowHeight);
+
+  starCount = map(width, 200, 6000, 500, 3000);
 
   for (var i = 0; i < starCount; i++) {
     stars[i] = new Star();
@@ -11,14 +17,56 @@ function setup() {
 }
 
 function draw() {
-  colorMode(HSL);
-  
   background(240, 40, 10);
+  colorMode(HSL);
+
+  const reducedMotion = window.matchMedia('(prefers-reduced-motion)');
+
   translate(width / 2, height / 2);
+
   for (var i = 0; i < stars.length; i++) {
-    stars[i].update();
+    if (!reducedMotion.matches) {
+      stars[i].update();
+    }
+
     stars[i].show();
   }
+
+  translate(-width / 2, -height / 2);
+
+  changeHeadingColor();
+
+  if (!reducedMotion.matches) {
+    observeButtons();
+  }
+}
+
+function changeHeadingColor() {
+  let heading = select(".section-content h1");
+
+  headingHue += .5;
+
+  if (headingHue == 360) {
+    headingHue = 0;
+  }
+
+  let headingColor = color(headingHue, 50, 50);
+
+  heading.style('color', headingColor);
+}
+
+function observeButtons() {
+  let buttonHovered = select(".a-button:hover");
+
+  if (buttonHovered) {
+    targetStarSpeed = 100;
+  } else {
+    targetStarSpeed = 1;
+  }
+
+  let deltaStarSpeed = targetStarSpeed - starSpeed;
+
+  starSpeed += deltaStarSpeed * starEasing;
 }
 
 function windowResized() {
